afbce2e3c7ee59c7beaa1382b878384aa04c08f4
mojila <mojila@github.com>
2018-01-29 20:04:12 +0700

    add proxy

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 var request = require("request");
+var proxy = require('./proxylist.js');
+
+var proxyRand = () => {
+  var text = "";
+  var possible = "0123456789";
+  
+  for (var i = 0; i < 1; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return 'http://' + proxy[text].ip + ':' + proxy[text].port;
+};
 
 // Random
 var rand = (option) => {
@@ -18,7 +29,7 @@ var rand = (option) => {
 
 var config = {
   // Link Referral
-  ref: 'https://drop.allsportschain.com/EN/index?r=9434f832'
+  ref: 'https://drop.allsportschain.com/EN/index?r=471b7bbe'
 };
 
 var data = {
@@ -26,28 +37,33 @@ var data = {
   sns: rand('telegram')
 };
 
-var send = (addr, sns) => {
-  request({
-    method: 'POST',
-    url: 'https://drop.allsportschain.com/inner/recorder/submit',
-    headers: {
-      'Cache-Control': 'no-cache',
-      Referer: config.ref,
-      'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
-    },
-    formData: {
-      addr: addr,
-      sns: sns
-    }
-  }, function (error, response, body) {
-    if (error) throw new Error(error);
-
-    return '';
-  });
-
-  return 'Sukses : ' + addr;
+var send = (addr, sns, proxy) => {
+  try {
+    request({
+      proxy: proxy,
+      method: 'POST',
+      url: 'https://drop.allsportschain.com/inner/recorder/submit',
+      headers: {
+        'Cache-Control': 'no-cache',
+        Referer: config.ref,
+        'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+      },
+      formData: {
+        addr: addr,
+        sns: sns
+      }
+    }, function (error, response, body) {
+      if (error) throw new Error(error);
+  
+      return '';
+    });
+  
+    return 'Sukses, proxy : ' + proxy;
+  } catch(e) {
+    return 'Gagal, proxy : ' + proxy;
+  }
 };
 
 setInterval(() => {
-  console.log(send(rand('address'), rand('telegram')));
-}, 2000);
+  console.log(send(rand('address'), rand('telegram'), proxyRand()));
+}, 5000);
